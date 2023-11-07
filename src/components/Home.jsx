778b11994ad02357c403ebcfdd3d8cd2866b2933
Dishant <dishantbaghel55@gmail.com>
2023-11-07 13:46:57 +0530

    third commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,31 +1,37 @@
 import React, { useEffect, useState } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircleCheck, faCirclePlus, faL, faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
-import { v4 as uuidv4 } from 'uuid';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleCheck,
+  faCirclePlus,
+  faL,
+  faPenToSquare,
+  faTrash,
+} from "@fortawesome/free-solid-svg-icons";
+import { v4 as uuidv4 } from "uuid";
 
-const getLocalItems =()=>{
-  let list = localStorage.getItem('lists')
+const getLocalItems = () => {
+  let list = localStorage.getItem("lists");
 
-  if(list){
-    return JSON.parse(localStorage.getItem('lists'));
-  }
-  else{
+  if (list) {
+    return JSON.parse(localStorage.getItem("lists"));
+  } else {
     return [];
   }
-}
+};
 
 const Home = () => {
   const [input, setInput] = useState("");
   const [todos, setTodos] = useState(getLocalItems());
   const [showInput, setShowInput] = useState(false);
-  const [editText, setEditText] = useState('');
+  const [editText, setEditText] = useState("");
   const [editedTodoId, setEditedTodoId] = useState(null);
   const [todoFilter, setTodoFilter] = useState("All");
-  const [err , setErr] = useState(false)
+  const [err, setErr] = useState(false);
+  const [activeButton, setActiveButton] = useState("All");
 
-  useEffect(()=>{
-    localStorage.setItem('lists',JSON.stringify(todos))
-  },[todos])
+  useEffect(() => {
+    localStorage.setItem("lists", JSON.stringify(todos));
+  }, [todos]);
 
   const handleShowInput = () => {
     setShowInput(true);
@@ -42,17 +48,14 @@ const Home = () => {
 
   const handleAdd = () => {
     if (input.trim() !== "") {
-      setTodos([ ...todos ,{ id: uuidv4(), text: input, completed: false }]);
+      setTodos([...todos, { id: uuidv4(), text: input, completed: false }]);
       setShowInput(false);
-      setErr(false)
+      setErr(false);
+    } else {
+      setErr(true);
     }
-    else{
-      setErr(true)
-    }
-    setInput('');
-    
+    setInput("");
   };
-
 
   const todoFilterPage = todos.filter((eachElement) => {
     if (todoFilter === "Completed") {
@@ -73,31 +76,26 @@ const Home = () => {
   };
 
   const handleEdit = (id) => {
-   
-      setEditedTodoId(id);
-      const todoToEdit = todos.find((todo) => todo.id === id);
-      setEditText(todoToEdit.text);
-    
-   
+    setEditedTodoId(id);
+    const todoToEdit = todos.find((todo) => todo.id === id);
+    setEditText(todoToEdit.text);
   };
 
   const handleSaveEdit = (id) => {
     if (editText.trim() !== "") {
       setTodos((prevTodos) =>
-      prevTodos.map((item) =>
-        item.id === id ? { ...item, text: editText } : item
-      )
-    );
-    setEditedTodoId(null);
-    setEditText("");
-    }
-    else{
-      setErr(true)
+        prevTodos.map((item) =>
+          item.id === id ? { ...item, text: editText } : item
+        )
+      );
+      setEditedTodoId(null);
+      setEditText("");
+    } else {
+      setErr(true);
       if (err) {
-        alert("please enter data")
+        alert("please enter data");
       }
     }
-  
   };
 
   const handleDelete = (id) => {
@@ -105,16 +103,20 @@ const Home = () => {
     setTodos(deleteTodo);
   };
 
-  const handleClearAll =()=>{
-    setTodos([])
-  }
+  const handleClearAll = () => {
+    setTodos([]);
+  };
 
   return (
     <div className="parent">
       <div className="child">
         <div className="container1">
           <div className="title">Today</div>
-          <FontAwesomeIcon className="btn" onClick={handleShowInput} icon={faCirclePlus} />
+          <FontAwesomeIcon
+            className="btn"
+            onClick={handleShowInput}
+            icon={faCirclePlus}
+          />
         </div>
 
         {showInput === true ? (
@@ -127,58 +129,120 @@ const Home = () => {
                 onChange={handleInput}
                 placeholder="enter data here..."
               ></textarea>
-      
-              {
-                err &&
-                <div style={{color : 'red'}}> Please Enter Data</div>
-              }
+
+              {err && <div style={{ color: "red" }}> Please Enter Data</div>}
             </div>
             <div className="pop-up-btns">
-              <div className="pop-up-btn" onClick={handleCancelInput}>Cancel</div>
-              <div className="pop-up-btn" onClick={handleAdd}><b>Done</b></div>
+              <div className="pop-up-btn" onClick={handleCancelInput}>
+                Cancel
+              </div>
+              <div className="pop-up-btn" onClick={handleAdd}>
+                <b>Done</b>
+              </div>
             </div>
           </div>
-        ) : (
+        ) : (todos.length > 0 &&
           <div>
             <div className="status">
-            <button className="status-btn" onClick={() => setTodoFilter("All")}>All</button>
-          <button className="status-btn" onClick={() => setTodoFilter("Completed")}>Completed</button>
-          <button className="status-btn" onClick={() => setTodoFilter("Incomplete")}>Incomplete</button>
+              <button
+                className={`status-btn ${
+                  activeButton === "All" ? "active" : ""
+                }`}
+                onClick={() => {
+                  setTodoFilter("All");
+                  setActiveButton("All");
+                }}
+              >
+                All
+              </button>
+              <button
+                className={`status-btn ${
+                  activeButton === "Completed" ? "active" : ""
+                }`}
+                onClick={() => {
+                  setTodoFilter("Completed");
+                  setActiveButton("Completed");
+                }}
+              >
+                Completed
+              </button>
+              <button
+                className={`status-btn ${
+                  activeButton === "Incomplete" ? "active" : ""
+                }`}
+                onClick={() => {
+                  setTodoFilter("Incomplete");
+                  setActiveButton("Incomplete");
+                }}
+              >
+                Incomplete
+              </button>
             </div>
             <ul className="all-todos">
               {todoFilterPage.map((e) => (
                 <li className="single-todo" key={e.id}>
-                  <input className="checkbox" type="checkbox" defaultChecked={e.completed} onChange={() => handleCheckbox(e.id)} />
                   <div>
-                  {e.completed ? (
-                    <s>{e.text}</s>
-                  ) : (
-                    <>
-                      {editedTodoId === e.id ? (
-                        <div className="edit-todo">
-                          <input
-                            className="edit-input"
-                            type="text"
-                            value={editText}
-                            onChange={(e) => setEditText(e.target.value)}
-                          />
-                          <FontAwesomeIcon className="btn" onClick={() => handleSaveEdit(e.id)} icon={faCircleCheck} />
-                        </div>
+                    <div className="abc">
+                      <input
+                        className="round-checkbox"
+                        type="checkbox"
+                        defaultChecked={e.completed}
+                        onChange={() => handleCheckbox(e.id)}
+                      />
+                      {e.completed ? (
+                        <s>{e.text}</s>
                       ) : (
-                        <div className="todos-text">{e.text}</div>
+                        <div>
+                          {editedTodoId === e.id ? (
+                            <form onSubmit={() => handleSaveEdit(e.id)}>
+                              <input
+                                className="edit-input"
+                                type="text"
+                                value={editText}
+                                onChange={(e) => setEditText(e.target.value)}
+                                onBlur={() => handleSaveEdit(e.id)}
+                              />
+                            </form>
+                          ) : (
+                            <div>{e.text}</div>
+                          )}
+                        </div>
                       )}
-                    </>
-                  )}</div>
-                  {editedTodoId !== e.id && (
-                    <FontAwesomeIcon  className="btn" onClick={() => handleEdit(e.id)} icon={faPenToSquare} />
-                  )}<FontAwesomeIcon className="btn" onClick={() => handleDelete(e.id)} icon={faTrash} />
-                  {e.completed ? ( <div className="complete"></div> ) : ( <div className="incomplete"></div> )}
-                </li>
-
-              ))}
-            </ul><div className="footer">
-                    <button onClick={handleClearAll} className="status-btn">Clear All</button>
+                    </div>
                   </div>
+                  <div className="todo-btns">
+                    {editedTodoId === e.id ? (
+                      <FontAwesomeIcon
+                        className="btn"
+                        onClick={() => handleSaveEdit(e.id)}
+                        icon={faCircleCheck}
+                      />
+                    ) : (
+                      <FontAwesomeIcon
+                        className="btn"
+                        onClick={() => handleEdit(e.id)}
+                        icon={faPenToSquare}
+                      />
+                    )}
+                    <FontAwesomeIcon
+                      className="btn"
+                      onClick={() => handleDelete(e.id)}
+                      icon={faTrash}
+                    />
+                    {e.completed ? (
+                      <div className="complete"></div>
+                    ) : (
+                      <div className="incomplete"></div>
+                    )}
+                  </div>
+                </li>
+              ))}
+            </ul>
+            <div className="footer">
+              <button onClick={handleClearAll} className="status-btn">
+                Clear All
+              </button>
+            </div>
           </div>
         )}
       </div>
